f81406a1392e0b31686f6286cf9f2247ee9d7228
jAcquire Kingdom <jacquire.kingdom@gmail.com>
2026-09-03 09:39:44 -0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,17 @@
 // src/vite.config.ts
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from "dotenv";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 
-dotenv.config({
-  quiet: true,
-});
-
-export default defineConfig(() => {
-  const { HOST, VITE_PORT } = process.env;
+export default defineConfig(({ mode }) => { 
+  const env = loadEnv(mode, process.cwd(), '');
 
   return {
     plugins: [
-      react(),
-      nodePolyfills({
-        protocolImports: true,
-      }),
+      react()
     ],
     server: {
-      host: HOST || "0.0.0.0",
-      port: parseInt(VITE_PORT || "5173", 10),
+      host: env.HOST || "0.0.0.0",
+      port: parseInt(env.VITE_PORT || "5173", 10),
     }, 
   };
 });
